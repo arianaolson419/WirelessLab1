1f13610e2bf4e33a979acf204e9c03580c95f794
kuannie1 <anne.ku4@gmail.com>
2019-02-15 00:13:56 -0500

    reverted back to my older script--sorry!

--- correct_qpsk_data.py
+++ correct_qpsk_data.py
@@ -9,58 +9,38 @@ symbol_period = 20
 signal_len = (N + num_header_symbols) * symbol_period
 original_data = np.fromfile("tx4.dat", dtype=np.float32)
 original_data = original_data[::2] + 1j*original_data[1::2]
-
 plt.suptitle("Original Data Real (above), Imaginary(below)")
 plt.subplot(2, 1, 1)
 plt.plot(np.real(original_data))
-plt.title('Real')
-plt.xlabel('Sample')
-plt.ylabel('Amplitude')
 plt.subplot(2, 1, 2)
 plt.plot(np.imag(original_data))
-plt.title('Imaginary')
-plt.xlabel('Sample')
-plt.ylabel('Amplitude')
 plt.show()
 
 # Open the file containing the received samples.
 tmp = np.fromfile("receiveQPSK_gain70.dat", dtype=np.float32)
 tmp_header = np.fromfile("tx4_header_only.dat", dtype=np.float32)
-tmp_data = np.fromfile("tx4_data_only.dat", dtype=np.float32)
-y = tmp[::2] + 1j * tmp[1::2]
+y = tmp[::2]+1j*tmp[1::2]
 mean_y = np.mean(y) # Correct for DC offset.
 y -= mean_y
-header = tmp_header[::2] + 1j * tmp_header[1:2:]
-data = (tmp_data[::2] + 1j * tmp_data[1:2:])[0:N * symbol_period]  # We changed the number of bits being read, so we have to cut off the data for comparison.
+header = tmp_header[::2] + 1j*tmp_header[1:2:]
 
-# Cut off the noise.
-start_cutoff = .5 * np.max(y)
-start_index = np.argmax(np.abs(y) > start_cutoff)
-end_index = int(start_index + signal_len)
-if (end_index > len(y) - 1):
-    end_index = len(y) - 1
+# cut off the noise 
+def cut_noise(input_signal):
+    start_cutoff = .5 * np.max(y)
+    rough_start_index = np.argmax(np.abs(y) > start_cutoff)
+    rough_end_index = int(rough_start_index + signal_len)
+    if (rough_end_index > len(y) - 1):
+        rough_end_index = len(y) - 1
 
-# Cross correlate with header to find the exact start of the signal.
-cross_corr = signal.correlate(y[start_index:end_index], header)
-peak_index = start_index + (np.argmax(cross_corr) - (num_header_symbols * symbol_period // 2)) # index of the largest peak in y
+    cross_corr = signal.correlate(input_signal[rough_start_index:rough_end_index], header)
+    peak_index = rough_start_index + np.argmax(cross_corr) # index of the largest peak in y
+    # Make sure ther is a peak from cross correlation
+    # plt.plot(np.abs(cross_corr))
+    # plt.show()
+    input_signal = input_signal[peak_index:peak_index + signal_len]
+    return input_signal
 
-# Make sure there is a peak from cross correlation
-plt.plot(np.abs(cross_corr))
-plt.show()
-
-# Take the signal from the start of the data to the known length of the data
-# isolate just the data part of the signal.
-print("Y data packet plot")
-y_tmp = np.copy(y)
-y_tmp[:start_index] = 0
-y_tmp[start_index + signal_len:] = 0
-plt.plot(y.real)
-plt.plot(y_tmp.real)
-plt.title('Compare signal')
-plt.show()
-y = y[start_index:start_index + signal_len]
-
-#y = y[peak_index:peak_index + signal_len]
+y = cut_noise(y)
 # plot y data packet -- real vs. imaginary
 plt.suptitle("original data -- Real (above), Imaginary(below)")
 plt.subplot(2, 1, 1)
@@ -69,29 +49,36 @@ plt.subplot(2, 1, 2)
 plt.plot(np.imag(y))
 plt.show()
 
+
 # Estimate the magnitude of the channel and divide the signal by this
 h_mag_est = np.sqrt(np.mean(np.square(y)))
 y_normalized = y / h_mag_est
 
-# Create s[k] by raising the signal to the 4th power.
-s = np.power(y_normalized, 4)
 
-# TODO: Try this with overlapping sections instead of discrete chunks?
-# Split the signal into 2 or 5 parts and calculate separate f_deltas for each one
-num_chunks = 2  # Be careful to use chunks that evenly divide the data.
-chunk_len = signal_len // num_chunks
-x_est = np.zeros(y_normalized.shape[-1], dtype='complex64')
-for i in range(num_chunks):
-    start = i * chunk_len
-    fft = np.fft.fft(s[start: start + chunk_len])
+# TODO: Split the signal into 2 or 5 parts and calculate separate f_deltas for each one
+
+num_parts = 1
+x_est = np.zeros(original_data.shape[-1], dtype=np.complex64)
+segmentLength = (len(y)//num_parts)
+
+for i in range(num_parts):
+    startIndex = i * segmentLength
+    endIndex = startIndex + segmentLength
+    y_segment = y[startIndex:endIndex]
+    h_mag_est = np.sqrt(np.mean(np.square(y_segment)))
+    y_normalized = y_segment / h_mag_est
+
+    # Create s[k] by raising the signal to the 4th power.
+    s = np.power(y_normalized, 4)
+    fft = np.fft.fft(s)
     shifted_fft = np.fft.fftshift(fft)
     freq_axis = np.linspace(-np.pi, np.pi, shifted_fft.shape[-1])
 
     # Plot FFT to see a peak near 0
-    print("FFT Plot {} / {}".format(i + 1, num_chunks))
-    #plt.suptitle("FFT Plot")
-    #plt.plot(freq_axis, np.abs(shifted_fft))
-    #plt.show()
+    print("FFT Plot")
+    plt.suptitle("FFT Plot")
+    plt.plot(freq_axis, np.abs(shifted_fft))
+    plt.show()
 
     # Get f_delta and theta from finding frequency value and peak height
     x_offset = freq_axis[np.argmax(shifted_fft)]
@@ -99,82 +86,45 @@ for i in range(num_chunks):
     f_delta  = (x_offset)/-4                   
     theta    = -1*np.angle(y_height)/4
 
-    # Estimate x by multiplying ~y exp(j(freq_est * k + theta_est.
-    psi = f_delta * np.arange(0, chunk_len)  + theta
-
-    print(y_normalized.dtype)
-    x_est[start: start + chunk_len] = y_normalized[start: start + chunk_len] * np.exp(1j * psi)
-
-# # Plot the real and imaginary waveforms
-print("real vs imaginary")
-plt.suptitle("received signal-- real (above) & imaginary (below)")
-plt.figure(1)
-plt.subplot(2, 1, 1)
-plt.plot(np.real(x_est))
-plt.subplot(2, 1, 2)
-plt.plot(np.imag(x_est))
-plt.show(1)
-
-# # Plot constellation
-print("constellation plot")
-#plt.figure(2)
-#plt.suptitle("received signal-- constellation plot")
-#plt.plot(np.real(x_est), np.imag(x_est), '.')
-#plt.show(2)
+    # # Estimate x by multiplying ~y exp(j(freq_est * k + theta_est.
+    psi = f_delta * np.arange(0,y_normalized.shape[-1])  + theta
+    x_est[startIndex:endIndex] = y_normalized * np.exp(1j * psi)
 
 
-# TODO: Rotate constellation by (2n +1) / 4.
-def rotate_constellation_quadrants(n, signal):
-    """Rotates the constellation by (2n + 1) / 4 in order to shift the quadrants
-    of the data.
+def turn_data_to_bits(input):
     """
-    return signal * np.exp(1j * (2 * n + 1) * np.pi / 4)
+    Takes in the input and samples every 20 bits. 
+    If the majority of the imag or real bits there are greater than 0, assign the index a 1
+    """
+    bitsequence = [0] * (2*len(original_data))
+    for coordinates_i in range(0, len(input), symbol_period):
+        coordinates = input[coordinates_i:coordinates_i+symbol_period]
+        
+        # seeking for majority positive, otherwise false
+        positive_real = (np.real(coordinates) > 0).sum() > (symbol_period//2) 
+        positive_imag = (np.imag(coordinates) > 0).sum() > (symbol_period//2)
 
-def parse_data(signal):
-    bitsequence = []
-    for i in range(signal.shape[-1] // 20):
-        start = symbol_period * i
-        end = start + symbol_period
-        real_bit = np.sign(np.mean(signal.real[start:end]))
-        imag_bit = np.sign(np.mean(signal.real[start:end]))
-        bitsequence.append(real_bit)
-        bitsequence.append(imag_bit)
+        if (positive_real):
+            bitsequence[(coordinates_i//20)] = 1
+        
+        if positive_imag:
+            bitsequence[(coordinates_i//20)+1] = 1
 
-    bitsequence = [0 if x == -1 else 1 for x in bitsequence]
-    return np.array(bitsequence)
+    return bitsequence
 
-#def turn_data_to_bits(signal):
-#    """
-#    Takes in the signal and samples every 20 bits. 
-#    If the majority of the imag or real bits there are greater than 0, assign the index a 1
-#    """
-#    bitsequence = [0] * (2*len(original_data))
-#    for coordinates_i in range(0, len(signal), symbol_period):
-#        coordinates = signal[coordinates_i:coordinates_i+symbol_period]
-#        
-#        # seeking for majority positive, otherwise false
-#        positive_real = (np.real(coordinates) > 0).sum() > (symbol_period//2) 
-#        positive_imag = (np.imag(coordinates) > 0).sum() > (symbol_period//2)
-#
-#        if (positive_real):
-#            bitsequence[(coordinates_i//20)] = 1
-#        
-#        if positive_imag:
-#            bitsequence[(coordinates_i//20)+1] = 1
-#
-#    return bitsequence
+originaldata_bits = turn_data_to_bits(original_data)
+receiveddata_bits = turn_data_to_bits(x_est)
+wrongbits = []
+for i in range(len(originaldata_bits)):
+    if originaldata_bits[i] != receiveddata_bits[i]:
+        wrongbits.append(i)
 
-# Rotate the signal
-x_est_r = rotate_constellation_quadrants(0, x_est)
+print("wrongbits: ", wrongbits)
+print("original data len: ", len(originaldata_bits))
+print(len(wrongbits)/len(originaldata_bits))
 
-# Parse the bits of the transmitted signal.
-header_bits = parse_data(header)
-data_bits = parse_data(data)
-tx_bits = np.concatenate((header_bits, data_bits))
 
-# Parse the bits of the estimated signal.
-x_est_bits = parse_data(x_est_r)
+plt.plot(wrongbits, ".")
+plt.show()
 
-# Calculate the persent correct.
-percent_correct = np.sum(np.equal(x_est_bits, tx_bits)) / x_est_bits.shape[-1]
-print(percent_correct)
+# TODO: Figure out why approximately half of the first 15000 bits are wrong
